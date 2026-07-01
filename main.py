b07806186f4a47ad14d1e698ae5b0991c131de73
Luis Daniel Vargas Rodríguez <5cvargas.luis@gmail.com>
2026-07-01 11:20:50 -0600

    Agregamos productos y el ticket de inventario se le agregaron más visuales

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 print("Bienvenido al Sistema de Inventario de la Tienda Laika_Games")
 
+print("<<<<<<------------------------------------------------->>>>>>>")
+
 from producto import Producto
 
-producto1 = Producto("Colección Completa Olocoons O2 Bimbo 2023-2025 21 Piezas Color", 999, 32)
+producto1 = Producto("Colección Completa Olocoons O2 Bimbo 2023-2025 21 Piezas Color", 1200, 32)
 
+producto2 = Producto("Colección Completa Olocoons O2 Bimbo 2025 21 Piezas Translúcidos", 1200, 30)
 
+producto1.mostrar_producto()
+
+print("------------------------------------------------------------")
+
+producto2.mostrar_producto()
 
 
 #Vamos a quitar esta parta para usar un método, simplificando todo
