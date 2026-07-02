c27f20ccdca93b0a50cf2a218db8defdc24f6c15
Luis Daniel Vargas Rodríguez <5cvargas.luis@gmail.com>
2026-07-02 11:56:49 -0600

    Limpieza de código, se elimina todo lo innecesario

--- inventario.py
+++ inventario.py
@@ -23,21 +23,7 @@ class Inventario:
      guardar_datos(self.productos)
 
      print("Producto agregado correctamente.")
-    #Esto se modifica
-    """
-    def agregar_producto(self, nombre, precio, stock):
-        nuevo_producto = Producto(nombre, precio, stock)
-        self.productos.append(nuevo_producto)
-        guardar_datos(self.productos)
-        print("Producto agregado correctamente.")
-    """
-    #Esto se modifica
-    """
-    def agregar_producto(self, nombre, precio, stock):
-        nuevo_producto = Producto(nombre, precio, stock)
-        self.productos.append(nuevo_producto)
-        print("Producto agregado correctamente.")
-    """
+   
     def mostrar_inventario(self):
         if not self.productos:
             print("El inventario está vacío.")
